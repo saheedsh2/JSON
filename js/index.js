674cdfdfad66bd3bd21435f12611483360b50ed6
saheed <saheedsh2@gmail.com>
2022-12-05 14:17:31 +0100

    committed

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
 // javascript for index.html
 
 const container = document.querySelector('.blogs');
+const searchForm = document.querySelector('.search');
 
-
-const renderPosts = async () => {
+const renderPosts = async (term) => {
     let uri = 'http://localhost:3000/posts?_sort=likes&_order=desc';
+
+    if (term) {
+        uri += `&q=${term}`;
+    }
+
 
     const res = await fetch(uri);
     const posts = await res.json();
@@ -24,6 +29,12 @@ const renderPosts = async () => {
 
     container.innerHTML = template;
 }
+
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    renderPosts(searchForm.term.value.trim());
+})
 
 
 
